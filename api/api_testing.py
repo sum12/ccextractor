7bd92a17d848be4902173f07879f9dc6c48b42e0
Diptanshu8 <diptanshuj@gmail.com>
2017-08-21 06:17:28 +0000

    modified templer to be called from recursive_tester.py. Added recursive_tester.py for generating SRTs of all files in a directory. WIP : recursive_tester.py

--- api/api_testing.py
+++ api/api_testing.py
@@ -3,19 +3,17 @@
 ###
 import ccextractor as cc
 import api_support
-from multiprocessing import Queue,Process,Event 
 import sys
-import time
 
-def templer():
+def templer(arg_count, arg_list):
     s =  cc.api_init_options()
     cc.check_configuration_file(s)
-    for i in sys.argv[1:]:
+    for i in arg_list:
         cc.api_add_param(s,str(i))
     #very mandatory for keeping a track of pythonapi call. Always must be set.
     cc.my_pythonapi(s, callback)
     
-    compile_ret = cc.compile_params(s,len(sys.argv[1:]));
+    compile_ret = cc.compile_params(s,arg_count);
     
     #very mandatory for keeping a track of pythonapi call. Always must be called so that the program knows that the call is from pythonapi.
     cc.call_from_python_api(s)
@@ -25,7 +23,9 @@ def templer():
 def callback(line):
     api_support.generate_output_srt(line)
 if __name__=="__main__":
-    templer()
+    arg_list = sys.argv[1:]
+    arg_count = len(arg_list)
+    templer(arg_count, arg_list)
 
 
 
